7df73894389caea262474cc11d7204a14e9e5600
Jeff Geerling <geerlingguy@mac.com>
2019-09-26 19:28:14 -0500

    Issue #1: Final fixes to get pip-autoremove uninstalled.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,9 +3,9 @@
 # Remove Ansible and its dependencies.
 pip3 install pip-autoremove
 ln -s /usr/bin/pip3 /usr/bin/pip
-pip-autoremove ansible -y
+/usr/local/bin/pip-autoremove ansible -y
 rm -f /usr/bin/pip
-pip3 uninstall pip-autoremove
+pip3 uninstall pip-autoremove -y
 
 # Zero out the rest of the free space using dd, then delete the written file.
 echo "Writing zeroes to free space (this could take a while)."
